fdbaf6e304c1e4280a8ec8ed834f1875ca164759
srcecde <chiragr83@gmail.com>
2017-03-05 18:15:39 +0530

    file formats

--- orgjunk.py
+++ orgjunk.py
@@ -1,26 +1,35 @@
-#!/usr/bin/env python3
+"""
+-*- coding: utf-8 -*-
+========================
+Python Lazy Junk Files Organizer
+========================
+========================
+"""
 import os
 from pathlib import Path
 
 
 DIRECTORIES = {
-    "html": [".html5", ".html", ".htm", ".xhtml"],
-    "images": [".jpeg", ".jpg", ".tiff", ".gif", ".bmp", ".png", ".bpg", "svg",
+    "HTML": [".html5", ".html", ".htm", ".xhtml"],
+    "IMAGES": [".jpeg", ".jpg", ".tiff", ".gif", ".bmp", ".png", ".bpg", "svg",
                ".heif", ".psd"],
-    "videos": [".avi", ".flv", ".wmv", ".mov", ".mp4", ".webm", ".vob", ".mng",
+    "VIDEOS": [".avi", ".flv", ".wmv", ".mov", ".mp4", ".webm", ".vob", ".mng",
                ".qt", ".mpg", ".mpeg", ".3gp"],
-    "documents": [".oxps", ".epub", ".pages", ".docx", ".doc", ".fdf", ".ods",
+    "DOCUMENTS": [".oxps", ".epub", ".pages", ".docx", ".doc", ".fdf", ".ods",
                   ".odt", ".pwi", ".xsn", ".xps", ".dotx", ".docm", ".dox",
                   ".rvg", ".rtf", ".rtfd", ".wpd", ".xls", ".xlsx", ".ppt",
                   "pptx"],
-    "archives": [".a", ".ar", ".cpio", ".iso", ".tar", ".gz", ".rz", ".7z",
+    "ARCHIVES": [".a", ".ar", ".cpio", ".iso", ".tar", ".gz", ".rz", ".7z",
                  ".dmg", ".rar", ".xar", ".zip"],
-    "audio": [".aac", ".aa", ".aac", ".dvf", ".m4a", ".m4b", ".m4p", ".mp3",
+    "AUDIO": [".aac", ".aa", ".aac", ".dvf", ".m4a", ".m4b", ".m4p", ".mp3",
               ".msv", "ogg", "oga", ".raw", ".vox", ".wav", ".wma"],
-    "plaintext": [".txt", ".in", ".out"],
-    "pdf": [".pdf"],
-    "python": [".py"],
-    "xml": [".xml"]
+    "PLAINTEXT": [".txt", ".in", ".out"],
+    "PDF": [".pdf"],
+    "PYTHON": [".py"],
+    "XML": [".xml"],
+    "EXE": [".exe"],
+    "SHELL": [".sh"]
+
 }
 
 
@@ -34,7 +43,7 @@ def organize_junk():
         if entry.is_dir():
             continue
         file_path = Path(entry)
-        file_format = file_path.suffix.lower() 
+        file_format = file_path.suffix.lower()
         if file_format in FILE_FORMATS:
             directory_path = Path(FILE_FORMATS[file_format])
             directory_path.mkdir(exist_ok=True)
